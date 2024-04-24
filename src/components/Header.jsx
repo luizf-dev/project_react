0893affaf63b3632ec869c8a7ab75a61b21d55d9
Luiz Fernando <luizfvidal3@hotmail.com>
2024-04-24 09:36:33 -0300

    estilos na navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,21 @@
 import React, {Component} from "react";
 import '../scss/header.scss';
 import { Link } from "react-router-dom";
+import { Braces } from "lucide-react";
 
 class Header extends Component{
 
     render(){
         return(
             <div>
-                <div className="navbar">
+                <div className="header">
                     <nav>
-                    <Link to="/"><a>Logo</a></Link>    
+                        <Link to="/">
+                            <a className="logo">
+                            <Braces/>
+                                Logo
+                            </a>
+                        </Link>    
                         <ul className="header-ul">
                             <li><Link to="/">Home</Link></li>
                             <li><Link to="/sobre">Sobre</Link></li>
